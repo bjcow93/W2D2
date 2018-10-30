bb63fb36745431a45f519fdb56790b0d42567157
fvkramer <krame490@gmail.com>
2018-10-29 17:48:42 -0700

    Update board position color and display board on Display#render

--- Chess/display.rb
+++ Chess/display.rb
@@ -2,9 +2,14 @@ require_relative 'board'
 require 'colorize'
 
 class Display
-  def initialize
+  def initialize(board)
     @cursor = Cursor.new([0,0], board)
+    #cursor_pos is an array
   end
+
+  #cursor_pos is an array
   def render
+    board[@cursor.cursor_pos].colorize(:red)
+    board.rows
   end
 end
