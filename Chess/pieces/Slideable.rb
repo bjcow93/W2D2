e17ca3977efaa931f8252c901439df81444e5d78
bjcow93 <benjcowden@gmail.com>
2018-10-30 15:42:08 -0700

    Define possible moves on Bishop#moves

--- Chess/pieces/Slideable.rb
+++ Chess/pieces/Slideable.rb
@@ -1,2 +1,16 @@
 module Slideable
-end 
+  VERTICAL = [[1,0], [-1,0]]
+  HORIZONTAL = [[0,1], [0, -1]]
+  DIAGONAL = [[1,1], [1,-1], [-1,1], [-1,-1]]
+
+  def moves(moves)
+    #moves is an arry
+    if   #QUEEN
+      possible_moves = VERTICAL + HORIZONTAL + DIAGONAL
+    elsif #Rook
+      possible_moves = VERTICAL + HORIZONTAL
+    else #BISHOP
+      possible_moves = DIAGONAL
+    end
+  end
+end
