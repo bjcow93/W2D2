f1fb39a43a34dce5c68393e14f21861107af969e
fvkramer <krame490@gmail.com>
2018-10-30 17:31:24 -0700

    Try valid move functionality

--- Chess/pieces/rook.rb
+++ Chess/pieces/rook.rb
@@ -19,7 +19,7 @@ class Rook < Piece
 
 
   def valid_move?(start_pos, end_pos)
-    if res.any? do |sub_move|
+    if @possible_moves.any? do |sub_move|
       start_pos[0] + sub_move[0] == end_pos[0] && start_pos[1] + sub_move[1] == end_pos[1]
       end
     end
